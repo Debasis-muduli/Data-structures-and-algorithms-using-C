6a9f6cd147c33ef952924c83418efeca6233c738
Debasis Muduli <debasismuduli48@gmail.com>
2023-06-28 20:08:36 +0530

    added code of implementation of queue using linked list

---
+++ 6.Linked list/queue_using_linkedlist.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int data;
+	struct node *next;
+};
+
+struct node *front=NULL,*rear=NULL;
+
+void enqueue(int data)
+{
+	struct node *newnode;
+	newnode=(struct node*)malloc(sizeof(struct node));
+	newnode->data=data;
+	newnode->next=NULL;
+
+	if(front==NULL && rear==NULL)
+	{
+		front=rear=newnode;
+	}
+	else
+	{
+		rear->next=newnode;
+		rear=rear->next;
+	}
+}
+void display()
+{
+	if (front==NULL && rear==NULL)
+	{
+		printf("Nothing to dispaly\n");
+	}
+	else
+	{
+		struct node *temp=front;
+		printf("HEAD-->");
+		while(temp!=NULL)
+		{
+			printf("%d-->",temp->data);
+			temp=temp->next;
+		}
+		printf("TAIL\n");
+	}
+}
+
+void dequeue()
+{
+	 struct node *temp=front;
+	 if (front==NULL)
+	 {
+		 printf("Queue is Empty\n");
+	 }
+	 else if(front==rear)
+	 {
+		 printf("Deleted item is %d\n",front->data);
+		 front=rear=NULL;
+	 }
+
+	 else
+	 {
+		 printf("deleted item is %d\n",temp->data);
+		 front=front->next;
+	 }
+}
+
+
+void main()
+{
+	int ch,num;
+	while(1)
+	{
+		printf("...Menu Driven Program...");
+		printf("0.exit\n1.enqueue\n2.dequeue\n3.display\n");
+		printf("enter choice:");
+		scanf("%d:",&ch);
+		switch(ch)
+		{
+			case 0:
+				exit(0);
+				break;
+			case 1:
+				printf("Enter a number:");
+				scanf("%d",&num);
+				enqueue(num);
+				break;
+			case 2:
+				dequeue();
+				break;
+			case 3:
+				display();
+				break;
+			}
+	}
+}
+
+	
+
